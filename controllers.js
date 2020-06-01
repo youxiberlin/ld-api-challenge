d77d2c9408291c28765a7a5ff7c94f63305c78f3
Yuki Sato <yuki.sato@lightcurve.io>
2020-06-01 10:32:26 +0200

    Add checkComplexity function

--- controllers.js
+++ controllers.js
@@ -1,5 +1,32 @@
+const Decimal = require('decimal.js');
+const Word = require('./models/word');
+const { formatSentence, splitToWords, splitToSentences } = require('./services/formatters');
+const { countNumOfNonLexicalWords } = require('./services/helpers');
+
+let wordsPool;
+
+const calculateLd = (inputText, nlWordsList) => {
+	const inputWordsArr = splitToWords(formatSentence(inputText));
+	const numOfNonLexicalWords = countNumOfNonLexicalWords(inputWordsArr, nlWordsList);
+	const overall_ld = new Decimal(numOfNonLexicalWords / inputWordsArr.length)
+	const fixedLd = overall_ld.toFixed(2);
+	return fixedLd;
+};
+
 const checkComplexity = async (req, res) => {
-	res.send('I am checkComplexity contoller');
+	if (!wordsPool) {
+		const wordsDocs = await Word.find({});
+		const wordsList = wordsDocs.map(doc => doc.word);
+		wordsPool = wordsList;
+	}
+
+	const overall_ld = calculateLd(req.body.text, wordsPool);
+
+	res.send({
+		data: {
+			overall_ld,
+		}
+	});
 };
 
 module.exports = {
