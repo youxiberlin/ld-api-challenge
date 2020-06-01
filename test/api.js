000e3b89b0917bd39703bf5d022d4069261cdccb
Yuki Sato <yuki.sato@lightcurve.io>
2020-06-01 11:40:20 +0200

    Add sample test data and invalid test cases

--- test/api.js
+++ test/api.js
@@ -1,6 +1,7 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const app = require('../app');
+const sampleData = require('./sampleData');
 
 const { expect } = chai;
 chai.use(chaiHttp);
@@ -12,11 +13,41 @@ describe('Ld-API', () => {
 				.post('/complexity')
 				.type('form')
 				.send({
-					'text': 'hello world. I am yuki sato. what a beautiful day'
+					'text': sampleData.regular
 				})
 				.end((err, res) => {
 					expect(res).to.have.status(200);
 					expect(res.body).to.have.all.keys('data');
+					done();
+				});
+		});
+	});
+
+	describe('in valid input', () => {
+		it('returns 400 and message when the input words exceeds the limit', (done) => {
+			chai.request(app)
+				.post('/complexity')
+				.type('form')
+				.send({
+					'text': sampleData.invalidWords
+				})
+				.end((err, res) => {
+					expect(res).to.have.status(400);
+					expect(res.body).to.have.all.keys('message', 'status');
+					done();
+				});
+		});
+
+		it('returns 400 and message when the input characters exceeds the limit', (done) => {
+			chai.request(app)
+				.post('/complexity')
+				.type('form')
+				.send({
+					'text': sampleData.invalidChars
+				})
+				.end((err, res) => {
+					expect(res).to.have.status(400);
+					expect(res.body).to.have.all.keys('message', 'status');
 					done();
 				});
 		});
